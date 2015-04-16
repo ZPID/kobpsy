bb13d1bb16750d79146740376c3ad3cac0df08f2
barth <barth@zpid.de>
2015-04-16 14:14:19 +0200

    Moved config and resources to common project.

--- se4ojs/sources/client/src/main/java/org/zpid/se4ojs/app/util/FilterUnprocessedPDFs.java
+++ se4ojs/sources/client/src/main/java/org/zpid/se4ojs/app/util/FilterUnprocessedPDFs.java
@@ -1,9 +1,7 @@
 package org.zpid.se4ojs.app.util;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.FileVisitResult;
