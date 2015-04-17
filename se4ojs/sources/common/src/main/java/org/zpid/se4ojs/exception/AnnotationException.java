ef55252b2b9cc620dc1468f4d7705d1457c9e0fe
barth <barth@zpid.de>
2015-04-17 13:22:24 +0200

    added exception package and annotationexceptionclass

---
+++ se4ojs/sources/common/src/main/java/org/zpid/se4ojs/exception/AnnotationException.java
@@ -0,0 +1,20 @@
+package org.zpid.se4ojs.exception;
+
+public class AnnotationException extends RuntimeException {
+
+	/**
+	 * The serial version uid.
+	 */
+	private static final long serialVersionUID = -2103546541381956263L;
+	
+
+	public AnnotationException(String message) {
+	}
+
+	public AnnotationException(String message, Throwable cause) {
+	}
+
+	public AnnotationException(Throwable cause) {
+	}
+
+}
