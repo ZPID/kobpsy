de201dba47e85ad22e77b6dacad5e31e5c8832b2
barth <barth@zpid.de>
2015-04-21 11:19:02 +0200

    added configuration exception.

---
+++ se4ojs/sources/common/src/main/java/org/zpid/se4ojs/exception/Se4ojsConfigurationException.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.zpid.se4ojs.exception;
+
+
+/**
+ * @author barth
+ *
+ */
+public class Se4ojsConfigurationException extends RuntimeException {
+
+	/**
+	 * serial version UID.
+	 */
+	private static final long serialVersionUID = 1752859224742740923L;
+
+	public Se4ojsConfigurationException() {
+		super();
+	}
+
+	public Se4ojsConfigurationException(String explanation) {
+		super(explanation);
+	}
+
+}
