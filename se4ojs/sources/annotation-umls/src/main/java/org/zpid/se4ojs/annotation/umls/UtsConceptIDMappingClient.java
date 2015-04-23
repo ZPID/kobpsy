2335f88810d751131cdc448de45290994b87e32a
barth <barth@zpid.de>
2015-04-23 14:58:38 +0200

    Added tests for concept name extraction for the conceptUri in bioportal. Changed the method that extracts the concept name for the uri from the preferred name.

--- se4ojs/sources/annotation-umls/src/main/java/org/zpid/se4ojs/annotation/umls/UtsConceptIDMappingClient.java
+++ se4ojs/sources/annotation-umls/src/main/java/org/zpid/se4ojs/annotation/umls/UtsConceptIDMappingClient.java
@@ -13,10 +13,13 @@ import gov.nih.nlm.uts.webservice.security.UtsWsSecurityControllerImplService;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.StringTokenizer;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.zpid.se4ojs.app.Config;
 import org.zpid.se4ojs.exception.AnnotationException;
@@ -34,63 +37,52 @@ import org.zpid.se4ojs.exception.AnnotationException;
  */
 public class UtsConceptIDMappingClient {
 	
-	private static final String UMLS_VERSION = Config.getUmlsVersionForUtsServices();
-	
-	// TODO move to Config
-	private static final String serviceName = "http://umlsks.nlm.nih.gov";
+	static final String DEFAULT_MULTI_WORD_DELIM = "_";
 
-	public static final int EXPIRY_INTERVALL = 8;
+	static final String UMLS_VERSION = Config.getUmlsVersionForUtsServices();
+
+	static final String OMIT_STOPWORDS = "stopWords";
 	
 	/** The singleton instance of this class. */
 	private static UtsConceptIDMappingClient utsConceptIDMappingClient;
 
-	/** The security service that obtains the tickets. */
-	private UtsWsSecurityController	utsSecurityService;
-
 	private Logger log = Logger.getLogger(UtsConceptIDMappingClient.class);
 
 	/** The set of ontologies. */
-	private Set<String> ontologies = Config.getUmlsOntologiesAsSet();
+	private OntologyMappingHandler mappingHandler;
 	
-	/** This ticket authenticates the user at UTS and is valid for 8 hours. */
-	private String proxyGrantTicket;
-	
-	private Date proxyGrantTicketExpiryTime;
-
 	/** Stores an object that passes properties to the service. */
 	private Psf psf;
 
-	private UtsWsContentController utsContentService;
+	private Map<String, Set<BOSourceConcept>> metaIdToSourceConcepts = new HashMap<String, Set<BOSourceConcept>>();
 
-	/**
-	 * Gets a ticket which authenticates the user at UTS and is valid for 8 hours.
-	 * 
-	 * @return the ticket
-	 * @throws UtsFault_Exception if ticket could not be obtained
-	 */
-	String createProxyGrantTicket() throws UtsFault_Exception {
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.HOUR_OF_DAY, EXPIRY_INTERVALL);
-		proxyGrantTicketExpiryTime = cal.getTime();
-		return utsSecurityService.getProxyGrantTicket(Config.getUMLSUsername(),
-						Config.getUMLSPassword());
-	}
-	
-	private UtsConceptIDMappingClient() throws UtsFault_Exception {
+	private Set<String> ontologies;
+
+	private UtsServiceTicketHandler utsServiceTicketHandler;
+
+	private UtsConceptIDMappingClient(OntologyMappingHandler mappingHandler, UtsServiceTicketHandler ticketHandler) throws UtsFault_Exception {
 		super();
+		this.mappingHandler = mappingHandler;
+		this.ontologies = mappingHandler.getOntologies();
+		this.utsServiceTicketHandler = ticketHandler;
 		init();
 	}
 	
-	private void init() throws UtsFault_Exception {
-		utsContentService = initServices();
+	void init() throws UtsFault_Exception {
+		utsServiceTicketHandler = initServices();
 		psf = createPsf();
 	}
 	
-	public static UtsConceptIDMappingClient getInstance() throws UtsFault_Exception {
+	public static UtsConceptIDMappingClient getInstance(OntologyMappingHandler mappingHandler) throws UtsFault_Exception {
 		if (utsConceptIDMappingClient != null) {
 			return utsConceptIDMappingClient;
 		}
-		return new UtsConceptIDMappingClient();
+		return new UtsConceptIDMappingClient(mappingHandler, null);
+	}
+
+	static UtsConceptIDMappingClient getInstance(OntologyMappingHandler ontologyMappingHandler,
+			UtsServiceTicketHandler ticketGetter) throws UtsFault_Exception {
+		return new UtsConceptIDMappingClient(ontologyMappingHandler, ticketGetter);
 	}
 
 	/**
@@ -100,54 +92,106 @@ public class UtsConceptIDMappingClient {
 	 * @param cui the cui to resolve
 	 * @return a map containing the sourceID concept and the acronym of the ontology it belongs to
 	 */
-	public Map<String, String> mapCuiToAtomIDs(String cui, String cuiPrefLabel) {
-		Map<String, String> atoms = new HashMap<>();
-	    
-		try {
-			List<AtomDTO> conceptAtoms = utsContentService.getConceptAtoms(getSingleUseTicket(), UMLS_VERSION, cui, psf);
-			log.debug("cui pref label: " + cuiPrefLabel);
-			boolean found = false;
-			for (AtomDTO atom : conceptAtoms) {
-				String rootSource = atom.getRootSource();
-				String ui = atom.getCode().getSourceUi();
-				if (ontologies.contains(rootSource)) {
-						found = true;
-						atoms.put(ui, rootSource);
-						System.out.println("id: " + ui);
-						System.out.println("source: " + atom.getRootSource() + "\n");
-						if (ui == null) {
-							log.warn("no id for concept: " + cui + "with term: " + cuiPrefLabel);
-						}
+	public Set<BOSourceConcept> mapCuiToAtomIDs(String cui, String cuiPrefLabel) {
+		Set<BOSourceConcept> sourceConcepts = metaIdToSourceConcepts.get(cui);
+	    if (sourceConcepts != null) {
+	    	return sourceConcepts;
+	    } else {
+	    	sourceConcepts = new HashSet<>();
+			try {
+				List<AtomDTO> conceptAtoms = utsServiceTicketHandler.getContentService().getConceptAtoms(
+						utsServiceTicketHandler.getSingleUseTicket(),
+						UMLS_VERSION, cui, psf);
+				log.info("cui pref label: " + cuiPrefLabel);
+				for (AtomDTO atom : conceptAtoms) {
+					String rootSource = atom.getRootSource();
+					String ui = "";
+					if (rootSource.equals("HL7V3.0")) {
+						ui = atom.getConcept().getUi();
+					} else {
+						ui = atom.getCode().getSourceUi();
+					}
+					if (ui == null) {
+						log.warn("no id for concept: " + cui + "with term: "
+								+ cuiPrefLabel);
+					} else {
 						log.info("id: " + ui);
-						log.info("source: " + atom.getRootSource());
-						log.info("pref terms: " + atom.getConcept().getDefaultPreferredName());
-						if (!cuiPrefLabel.equals(atom.getConcept().getDefaultPreferredName())) {
-							log.info("Pref terms differ!");
+						log.info("source: " + rootSource);
+						String sourcePreferredName = atom.getCode().getDefaultPreferredName();
+						log.info("pref term: "
+								+ extractConceptName(rootSource, sourcePreferredName));
+						log.info("term type: " + atom.getTermType());
+						if (!cuiPrefLabel.equals(extractConceptName(rootSource, sourcePreferredName))) {
+							log.warn("Pref terms differ!");
 						}
-						log.info("tree position count: " + atom.getTreePositionCount());
+						sourceConcepts.add(new BOSourceConcept(
+								ui, extractConceptName(rootSource, sourcePreferredName),
+								rootSource, atom.getTermType()));
+					}
+				}
+			} catch (gov.nih.nlm.uts.webservice.content.UtsFault_Exception e) {
+				log.error(("Error mapping UMLS metathesaurus concept (cui / label) to source vocab ids:"
+						+ " " + cui + cuiPrefLabel));
+				e.printStackTrace();
+			}	
+	    }
+	    metaIdToSourceConcepts.put(cui, sourceConcepts);
+		return sourceConcepts;
+	}
+
+	/**
+	 * Capitalizes the concept name.
+	 * Uses the specified delimiter for multi word concepts.
+	 * 
+	 * @param source the UMLS abbr. of the ontology source
+	 * @param defaultPrefName the preferred name of the concept within the source ontology
+	 * @return the concept name as it is used in the Concept URI.
+	 */
+	String extractConceptName(String source, String defaultPrefName) {
+		log.info("source default pref name: " + defaultPrefName);
+		String prefNameToUri = defaultPrefName.replace(" ", DEFAULT_MULTI_WORD_DELIM);
+		if (prefNameToUri.compareTo(defaultPrefName) != 0) {
+			OntologyMappingNcboUmls sourceMapping = mappingHandler.getSource(source);
+			String delim = sourceMapping.getMultiWordDelim();
+			if (StringUtils.isEmpty(delim)) {
+				delim = "";
+			}
+			StringTokenizer tokenizer = new StringTokenizer(prefNameToUri,
+					DEFAULT_MULTI_WORD_DELIM, true);
+			StringBuilder modPrefName = new StringBuilder();
+			while (tokenizer.hasMoreTokens()) {
+				String token = tokenizer.nextToken();
+				if (token.compareTo(DEFAULT_MULTI_WORD_DELIM) != 0 &&
+						sourceMapping.getStopwords().equals(OMIT_STOPWORDS) && token.length() <= 2) {
+					log.info("omitted stopword: " + token + "source: " + source + "term" + defaultPrefName);
+				} else{
+					String firstLetter = token.substring(0,1);
+					modPrefName.append(token.replace(firstLetter, firstLetter.toUpperCase()));
 				}
 			}	
-			if (!found) {
-				log.warn("No atoms found for CUI: " + cui + "with name : " + cuiPrefLabel);
+			String adaptedPrefName = modPrefName.toString();
+			adaptedPrefName = adaptedPrefName.replace("__", "_");
+			if (delim != DEFAULT_MULTI_WORD_DELIM) {
+				 adaptedPrefName = adaptedPrefName.replace(DEFAULT_MULTI_WORD_DELIM, delim);
 			}
-		} catch (gov.nih.nlm.uts.webservice.content.UtsFault_Exception e) {
-			log.error(("Error mapping UMLS metathesaurus concept (cui / label) to source vocab ids:"
-					+ " " + cui + cuiPrefLabel));
-			e.printStackTrace();
-		}	
-		return atoms;
+			return adaptedPrefName;
+		}
+
+		return prefNameToUri;
 	}
 
 	/**
 	 * Creates an object that stores properties to be adhered to by the web service.
 	 * 
-	 * @return the intialized psf object
+	 * @return the initialized psf object
 	 */
 	private Psf createPsf() {
 		Psf psf = new Psf();
+		psf.getIncludedSources().addAll(ontologies);		
+
 //		psf.setCaseSensitive(false);
 //		psf.setIncludedLanguage("en"); //TODO use config prop?
-//		psf.setIncludeObsolete(false);
+		psf.setIncludeObsolete(false);
 		return psf;
 	}
 
@@ -159,24 +203,17 @@ public class UtsConceptIDMappingClient {
 	 * @return the initialized content service
 	 * @throws UtsFault_Exception 
 	 */
-	private UtsWsContentController initServices() throws UtsFault_Exception {
-		utsSecurityService = 
-				(new UtsWsSecurityControllerImplService()).getUtsWsSecurityControllerImplPort();
+	UtsServiceTicketHandler initServices() throws UtsFault_Exception {
 		
-		proxyGrantTicket = createProxyGrantTicket();
-		
-		utsContentService = (new UtsWsContentControllerImplService())
-				.getUtsWsContentControllerImplPort();
-		
-		
-		UtsWsMetadataController utsMetadataService = null;
+		if (utsServiceTicketHandler == null) {
+			utsServiceTicketHandler = new UtsServiceTicketHandler();
+		}
+		utsServiceTicketHandler.createProxyGrantTicket();
 
-		utsMetadataService = (new UtsWsMetadataControllerImplService())
-				.getUtsWsMetadataControllerImplPort();
 
 		String allUMLSVersions = "";
 		try {
-			allUMLSVersions = utsMetadataService.getAllUMLSVersions(getSingleUseTicket());
+			allUMLSVersions = utsServiceTicketHandler.getAllUMLSVersions();
 			if(UMLS_VERSION == null || !allUMLSVersions.contains(UMLS_VERSION)) {
 				throw new AnnotationException("Wrong UMLS Version for UTS serices specified. Please check the configuration.");
 			}
@@ -185,16 +222,75 @@ public class UtsConceptIDMappingClient {
 					+ e.getLocalizedMessage());
 			e.printStackTrace();
 		}
-		return utsContentService;
+		return utsServiceTicketHandler;
+	}
+
+	/**
+	 * @return the proxyGrantTicketGetter
+	 */
+	UtsServiceTicketHandler getUtsServiceTicketHandler() {
+		return utsServiceTicketHandler;
 	}
 	
+}
+
+class UtsServiceTicketHandler {
+	
+	// TODO move to Config
+	private static final String UTS_SERVICE_BASE_URI = "http://umlsks.nlm.nih.gov";
+	
+	public static final int EXPIRY_INTERVALL = 8;
+	
+	private Date proxyGrantTicketExpiryTime;
+	
+	private Logger log = Logger.getLogger(UtsServiceTicketHandler.class);
+	
+	/** The security service that obtains the tickets. */
+	private UtsWsSecurityController	utsSecurityService;
+	
+	/** This ticket authenticates the user at UTS and is valid for 8 hours. */
+	private String proxyGrantTicket;
+
+	/**
+	 * Gets a ticket which authenticates the user at UTS and is valid for 8 hours.
+	 * 
+	 * @return the ticket
+	 * @throws UtsFault_Exception if ticket could not be obtained
+	 */
+	String createProxyGrantTicket() throws UtsFault_Exception {
+		
+		utsSecurityService = 
+				(new UtsWsSecurityControllerImplService()).getUtsWsSecurityControllerImplPort();
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.HOUR_OF_DAY, EXPIRY_INTERVALL);
+		proxyGrantTicketExpiryTime = cal.getTime();
+		proxyGrantTicket = utsSecurityService.getProxyGrantTicket(Config.getUMLSUsername(),
+						Config.getUMLSPassword());
+		return proxyGrantTicket;
+	}
+	
+	public String getAllUMLSVersions() throws gov.nih.nlm.uts.webservice.metadata.UtsFault_Exception {
+		return getMetadataService().getAllUMLSVersions(getSingleUseTicket());
+	}
+
+	UtsWsMetadataController getMetadataService() {
+		return (new UtsWsMetadataControllerImplService())
+				.getUtsWsMetadataControllerImplPort();
+	}
+
+	UtsWsContentController getContentService() {
+		return (new UtsWsContentControllerImplService())
+		   .getUtsWsContentControllerImplPort();
+	}
+
 	String getSingleUseTicket() {
+		
 		try {
 			if (getDate().compareTo(proxyGrantTicketExpiryTime)  > 0) {
 				proxyGrantTicket = createProxyGrantTicket();
 			}
 			return utsSecurityService.getProxyTicket(proxyGrantTicket,
-					serviceName);
+					UTS_SERVICE_BASE_URI);
 		} catch (Exception e) {
 			//no exception thrown, because might be a one-off problem and users might be able to live with
 			//incomplete annotations rather than interrupting the program.
@@ -202,19 +298,11 @@ public class UtsConceptIDMappingClient {
 			return null;
 		}
 	}
-
+	
+	
 	Date getDate() {
 		return new Date();
 	}
-
-	/**
-	 * Just for testing..
-	 * 
-	 * @param args
-	 * @throws UtsFault_Exception
-	 */
-	public static void main(String[] args) throws UtsFault_Exception {
-		UtsConceptIDMappingClient mappingClient = new UtsConceptIDMappingClient();
-		mappingClient.mapCuiToAtomIDs("C0870087", "Abuse of Power");
-	}
+	
+	
 }
--- se4ojs/sources/annotation-umls/src/test/java/org/zpid/se4ojs/annotation/umls/TestUtsConceptIDMappingClient.java
+++ se4ojs/sources/annotation-umls/src/test/java/org/zpid/se4ojs/annotation/umls/TestUtsConceptIDMappingClient.java
@@ -4,16 +4,18 @@ package org.zpid.se4ojs.annotation.umls;
  */
 
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import gov.nih.nlm.uts.webservice.security.UtsFault_Exception;
 
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.mockito.Mockito.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -25,33 +27,37 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest( {UtsConceptIDMappingClient.class })
 public class TestUtsConceptIDMappingClient {
-
-	private UtsConceptIDMappingClient utsConceptIDMappingClient;
 	
-	/**
-	 * @throws java.lang.Exception
-	 */
+	private UtsConceptIDMappingClient utsConceptIDMappingClient;
+	private OntologyMappingHandler mappingHandler;
+
 	@Before
-	public void setUp() throws Exception {
-
+	public void setUpBeforeClass() throws UtsFault_Exception, gov.nih.nlm.uts.webservice.metadata.UtsFault_Exception {
+		UtsServiceTicketHandler ticketHandler = mock(UtsServiceTicketHandler.class);
+		Mockito.when(ticketHandler.getAllUMLSVersions()).thenReturn(UtsConceptIDMappingClient.UMLS_VERSION);
+		mappingHandler = mock(OntologyMappingHandler.class);
+		utsConceptIDMappingClient = PowerMockito.spy(UtsConceptIDMappingClient.getInstance(
+				mappingHandler, ticketHandler));
 	}
-
+	
 	/**
 	 * Test method for {@link org.zpid.se4ojs.annotation.umls.UtsConceptIDMappingClient#createProxyGrantTicket()}.
 	 * @throws UtsFault_Exception 
 	 */
 	@Test
 	public void testCreateProxyGrantTicket_isRenewedWhenExpiring() throws UtsFault_Exception {
-		utsConceptIDMappingClient = PowerMockito.spy(UtsConceptIDMappingClient.getInstance());
+		UtsServiceTicketHandler ticketHandler = PowerMockito.spy(new UtsServiceTicketHandler());
+		utsConceptIDMappingClient = PowerMockito.spy(UtsConceptIDMappingClient.getInstance(
+				mock(OntologyMappingHandler.class), ticketHandler));
 		
 		Date date = new Date();
 		Calendar cal  = Calendar.getInstance();
-		cal.add(Calendar.HOUR_OF_DAY, UtsConceptIDMappingClient.EXPIRY_INTERVALL);
-		assert(utsConceptIDMappingClient.getDate().compareTo(date) < 0);
+		cal.add(Calendar.HOUR_OF_DAY, UtsServiceTicketHandler.EXPIRY_INTERVALL);
+		assert(utsConceptIDMappingClient.getUtsServiceTicketHandler().getDate().compareTo(date) < 0);
 		
-		PowerMockito.when(utsConceptIDMappingClient.getDate()).thenReturn(cal.getTime()); 
-		utsConceptIDMappingClient.getSingleUseTicket();
-		verify(utsConceptIDMappingClient).createProxyGrantTicket();
+		Mockito.when(ticketHandler.getDate()).thenReturn(cal.getTime()); 
+		utsConceptIDMappingClient.getUtsServiceTicketHandler().getSingleUseTicket();
+		verify(ticketHandler, Mockito.times(2)).createProxyGrantTicket();
 	}
 	
 	/**
@@ -60,18 +66,78 @@ public class TestUtsConceptIDMappingClient {
 	 */
 	@Test
 	public void testCreateProxyGrantTicket_isNotRenewedWhenNotExpiring() throws UtsFault_Exception {
+		UtsServiceTicketHandler ticketHandler = PowerMockito.spy(new UtsServiceTicketHandler());
+		utsConceptIDMappingClient = PowerMockito.spy(UtsConceptIDMappingClient.getInstance(
+				mock(OntologyMappingHandler.class), ticketHandler));
 		
 		Date date = new Date();
 		Calendar cal  = Calendar.getInstance();
-		cal.add(Calendar.HOUR_OF_DAY, UtsConceptIDMappingClient.EXPIRY_INTERVALL);
+		cal.add(Calendar.HOUR_OF_DAY, UtsServiceTicketHandler.EXPIRY_INTERVALL - 1);
 		
-		utsConceptIDMappingClient = PowerMockito.spy(UtsConceptIDMappingClient.getInstance());
-		assert(utsConceptIDMappingClient.getDate().compareTo(date) > 0);
+		assert(utsConceptIDMappingClient.getUtsServiceTicketHandler().getDate().compareTo(date) > 0);
 		
-		PowerMockito.when(utsConceptIDMappingClient.getDate()).thenReturn(cal.getTime()); 
-		utsConceptIDMappingClient.getSingleUseTicket();
-		verify(utsConceptIDMappingClient, never()).createProxyGrantTicket();
+		Mockito.when(ticketHandler.getDate()).thenReturn(cal.getTime());  
+		utsConceptIDMappingClient.getUtsServiceTicketHandler().getSingleUseTicket();
+		verify(ticketHandler).createProxyGrantTicket();
 	}
+	
+	@Test
+	public void testExtractConceptName_MultiWord_Stopwords() {
+		String conceptNameToAdapt = "abuse of power";
+		String expectedConceptName = "Abuse_Power";
+		
+		OntologyMappingNcboUmls mapping = mock(OntologyMappingNcboUmls.class);
+		Mockito.when(mapping.getMultiWordDelim()).thenReturn(UtsConceptIDMappingClient.DEFAULT_MULTI_WORD_DELIM);
+		Mockito.when(mapping.getConceptSeparator()).thenReturn(UtsConceptIDMappingClient.DEFAULT_MULTI_WORD_DELIM);
+		Mockito.when(mapping.getStopwords()).thenReturn(UtsConceptIDMappingClient.OMIT_STOPWORDS);
+		Mockito.when(mappingHandler.getSource("PSY")).thenReturn(mapping);
+		
+		String extractedConceptName = utsConceptIDMappingClient.extractConceptName("PSY", conceptNameToAdapt);
+		assertEquals("Wrong concept name extracted", expectedConceptName, extractedConceptName);
+	}
+	
+	@Test
+	public void testExtractConceptName_Capitalize_NoStopwords() throws UtsFault_Exception {
+		
+		String conceptNameToAdapt = "abuse of power";
+		String expectedConceptName = "Abuse?Of?Power";
+		
+		OntologyMappingNcboUmls mapping = mock(OntologyMappingNcboUmls.class);
+		Mockito.when(mapping.getMultiWordDelim()).thenReturn("?");
+		Mockito.when(mapping.getConceptSeparator()).thenReturn("");
+		Mockito.when(mapping.getStopwords()).thenReturn("");
+		Mockito.when(mappingHandler.getSource("PSY")).thenReturn(mapping);
 
+		String extractedConceptName = utsConceptIDMappingClient.extractConceptName("PSY", conceptNameToAdapt);
+		assertEquals("Wrong concept name extracted", expectedConceptName, extractedConceptName);
+	}
+	
+	@Test
+	public void testExtractConceptName_NoMultiWordDelim() throws UtsFault_Exception {
+		String conceptNameToAdapt = "abuse of power";
+		String expectedConceptName = "AbuseOfPower";
+		
+		OntologyMappingNcboUmls mapping = mock(OntologyMappingNcboUmls.class);
+		Mockito.when(mapping.getMultiWordDelim()).thenReturn("");
+		Mockito.when(mapping.getConceptSeparator()).thenReturn("");
+		Mockito.when(mapping.getStopwords()).thenReturn("");
+		Mockito.when(mappingHandler.getSource("PSY")).thenReturn(mapping);
+		String extractedConceptName = utsConceptIDMappingClient.extractConceptName("PSY", conceptNameToAdapt);
+		assertEquals("Wrong concept name extracted", expectedConceptName, extractedConceptName);
+	}
+	@Test
+	public void testExtractConceptName_TestAPAConceptWBraces() throws UtsFault_Exception {
+		String conceptNameToAdapt = "lead (Metal)";
+		String expectedConceptName = "Lead_(Metal)";
+		
+		OntologyMappingNcboUmls mapping = mock(OntologyMappingNcboUmls.class);
+//		%23, P, _ , stopWords
+		Mockito.when(mapping.getMultiWordDelim()).thenReturn("_");
+		Mockito.when(mapping.getConceptSeparator()).thenReturn("%23");
+		Mockito.when(mapping.getStopwords()).thenReturn("stopWords");
+		Mockito.when(mappingHandler.getSource("PSY")).thenReturn(mapping);
+		String extractedConceptName = utsConceptIDMappingClient.extractConceptName("PSY", conceptNameToAdapt);
+		assertEquals("Wrong concept name extracted", expectedConceptName, extractedConceptName);
+	}
+	
 }
-
