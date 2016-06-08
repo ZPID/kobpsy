928b70e53d017cb16ad75df0459931469fa5f59c
barth <barth@zpid.de>
2016-06-08 14:40:15 +0200

    adapted logger creation to log4j2

--- se4ojs/sources/textStructure/src/main/java/org/zpid/se4ojs/textStructure/StructureTransformer.java
+++ se4ojs/sources/textStructure/src/main/java/org/zpid/se4ojs/textStructure/StructureTransformer.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jdom2.Document;
